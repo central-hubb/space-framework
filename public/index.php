efb533bd59200630cb36262aac7180f36e6bb7b5
Daniel Lee <mrdaniellee2020@gmail.com>
2018-11-04 13:23:15 +0000

    autoload fix

--- public/index.php
+++ public/index.php
@@ -7,11 +7,11 @@ ini_set('display_errors',1);
 
 // set autoloader to require all class files automatically
 spl_autoload_register(function ($class_name) {
-	str_replace('app', 'App', $class_name);
+	str_replace('App', 'app', $class_name);
 	$filename = '../'.str_replace('\\', '/', $class_name) . '.php';
 	echo $filename;
 	exit;
-	
+
 	if(file_exists($filename)) {
 		include $filename;
 	}
