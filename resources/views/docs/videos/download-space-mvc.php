fb6a7d96818e0a4d7d5b0bc0b703b4824b576d72
Daniel Lee <mrdaniellee2020@gmail.com>
2018-11-20 00:22:49 +0000

    updated documentation and added mailist and help pages to frontend

--- resources/views/docs/videos/download-space-mvc.php
+++ resources/views/docs/videos/download-space-mvc.php
@@ -1,5 +1,5 @@
 <h1>How to download space mvc using source tree?</h1>
 
-<p>We are currently preparing this video now it we will have it created by 21st November 2018</p>
+<p>We are currently preparing this video now we will have it created ready by 21st November 2018</p>
 
 <p>Thanks for your patience and we hope you have enjoyed these tutorial videos so far!</p>
