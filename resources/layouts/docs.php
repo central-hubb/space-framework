0620ddcf8fedf4b7d1740632f1b2144df5d3d4d1
Daniel Lee <mrdaniellee2020@gmail.com>
2018-11-21 01:18:12 +0000

    added disqus comments to docs theme

--- resources/layouts/docs.php
+++ resources/layouts/docs.php
@@ -53,6 +53,8 @@
 		</div>
 		<div class="col-md-8">
 			<?php echo $content;?>
+
+            <div id="disqus_thread" style="margin-top:120px;"></div>
 		</div>
 	</div>
 	<hr />
@@ -74,6 +76,20 @@
 <script src="/bower_components/rainbow/dist/rainbow.min.js"></script>
 <script src="/bower_components/rainbow/src/language/generic.js"></script>
 <script src="/bower_components/rainbow/src/language/php.js"></script>
+
+<script>
+	var disqus_config = function () {
+	this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+	this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+	};
+    (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://space-mvc.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
 </body>
 </html>
