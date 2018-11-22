24b59146b3cc1ede7306590cb15547003a3e27a4
knrick <kndrick@yandex.ru>
2018-11-23 00:35:14 +0300

    cleaned up getting-started section

--- resources/views/docs/getting-started/configuration.php
+++ resources/views/docs/getting-started/configuration.php
@@ -1,6 +1,5 @@
 <article>
     <h2>Configuration</h2>
-    <p>All of the configuration files for the Space MVC framework are stored in the <code
-                class=" language-php">config</code> directory. Each option is documented, so feel free to look through
+    <p>All of the configuration files for the Space MVC framework are stored in the config directory. Each option is documented, so feel free to look through
         the files and get familiar with the options available to you.</p>
 </article>
